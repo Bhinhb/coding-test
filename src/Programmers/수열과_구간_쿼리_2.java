da45064e5eb47e4e7855ea60bb27d90193818cff
Bhin <bbhinhb@gmail.com>
2025-07-15 18:38:52 +0900

    feat: Day7 ~ Day8
    - 오늘꺼 정말 어렵다 한 번 더 공부하자

--- src/Programmers/수열과_구간_쿼리_2.java
+++ src/Programmers/수열과_구간_쿼리_2.java
@@ -6,6 +6,7 @@ package Programmers;
 //  각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 k보다 크면서 가장 작은 arr[i]를 찾습니다.
 //  각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요.
 //  단, 특정 쿼리의 답이 존재하지 않으면 -1을 저장합니다.
+
 public class 수열과_구간_쿼리_2 {
     public int[] solution(int[] arr, int[][] queries) {
         int[] result = new int[queries.length];
