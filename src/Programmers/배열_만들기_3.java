7827e293a7bc5915289cbf8c9b17f032938615f5
Bhin <bbhinhb@gmail.com>
2025-07-21 14:17:11 +0900

    feat: Programmers Lv.0
    - n개의 간격의 원소들
    - 배열 만들기 3
    - 수열과 구간 쿼리 1
    - 왼쪽 오른쪽
    - 홀수 vs 짝수

--- src/Programmers/배열_만들기_3.java
+++ src/Programmers/배열_만들기_3.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 //  intervals는 항상 [[a1, b1], [a2, b2]]의 꼴로 주어지며 각 구간은 닫힌 구간입니다.
 //  닫힌 구간은 양 끝값과 그 사이의 값을 모두 포함하는 구간을 의미합니다.
 //  이때 배열 arr의 첫 번째 구간에 해당하는 배열과 두 번째 구간에 해당하는 배열을 앞뒤로 붙여 새로운 배열을 만들어 return 하는 solution 함수를 완성해 주세요.
-public class 배열_만들기 {
+public class 배열_만들기_3 {
     public int[] solution(int[] arr, int[][] intervals) {
         int a1 = intervals[0][0];
         int b1 = intervals[0][1];
