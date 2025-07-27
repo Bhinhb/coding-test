abfb19d7f21b89bb989e8a6a91f629a6406d0a2a
Bhin <bbhinhb@gmail.com>
2025-07-28 01:15:18 +0900

    feat: Programmers Lv.0
    - ad 제거하기
    - rny_string
    - x 사이의 갯수
    - 간단한 식 계산하기
    - 공백으로 구분하기 1
    - 무작위로 k개의 수 뽑기
    - 문자열 잘라서 정렬하기
    - 문자열이 몇변 등장하는지 세기
    - 배열 만들기 6
    - 배열의 원소만큼 추가하기
    - 빈 배열에 추가 삭제하기
    - 세 개의 구분자
    - 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기

---
+++ src/Programmers/문자열이_몇번_등장하는지_세기.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+
+//  문제 설명
+//  문자열 myString과 pat이 주어집니다. myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+public class 문자열이_몇번_등장하는지_세기 {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        int index = 0;
+
+        while ((index = myString.indexOf(pat, index)) != -1) {
+            answer++;
+            index++;
+        }
+        return answer;
+    }
+}
---
+++ src/Programmers/배열의_원소만큼_추가하기.java
@@ -0,0 +1,32 @@
+package Programmers;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+//  문제 설명
+//  아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 양의 정수 배열 arr가 매개변수로 주어질 때,
+//  arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
+public class 배열의_원소만큼_추가하기 {
+    public int[] solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                answer.add(arr[i]);
+            }
+        }
+        int[] result  = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        return result;
+    }
+
+    public int[] solution2(int[] arr) {
+        return Arrays.stream(arr)
+                .flatMap(a -> IntStream.range(0, a).map(i -> a))
+                .toArray();
+    }
+}
